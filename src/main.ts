d8efb04d34b5a00aa8af5c7059bdce5853a41813
DemaPy <benderdema@gmail.com>
2025-06-29 18:04:57 +0200

    feat: use port 8000

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   });
   app.use(cookieParser());
   console.log(process.env.PORT);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(8000);
 }
 bootstrap();
