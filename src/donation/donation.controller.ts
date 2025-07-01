db1ec010a60276cc491ee9a0099730807e206d36
DemaPy <benderdema@gmail.com>
2025-07-01 08:35:52 +0200

    feat: add throw; clear cookie if voted and feature created

--- src/donation/donation.controller.ts
+++ src/donation/donation.controller.ts
@@ -22,35 +22,47 @@ export class DonationController {
     @Query('session_id') sessionId: string,
     @Res() res: Response,
   ) {
-    if (!sessionId) {
-      throw new HttpException('Missing session_id', HttpStatus.BAD_REQUEST);
-    }
-    const isValid = await this.stripeService.verifySession(sessionId);
-    if (!isValid) {
-      return res.status(403).json({ success: false });
-    }
-    const hasSubmittedFeature = await this.prisma.feature.findUnique({
-      where: {
-        sessionId,
-      },
-    });
+    try {
+      if (!sessionId) {
+        throw new HttpException('Missing session_id', HttpStatus.BAD_REQUEST);
+      }
+      const isValid = await this.stripeService.verifySession(sessionId);
+      if (!isValid) {
+        return res.status(403).json({ success: false });
+      }
+      const hasSubmittedFeature = await this.prisma.feature.findUnique({
+        where: {
+          sessionId,
+        },
+      });
 
-    const hasVoted = await this.prisma.featureLike.findUnique({
-      where: {
-        sessionId,
-      },
-    });
+      const hasVoted = await this.prisma.featureLike.findUnique({
+        where: {
+          sessionId,
+        },
+      });
+      if (hasSubmittedFeature && hasVoted) {
+        res.clearCookie('sessionId');
+      } else {
+        res.cookie('sessionId', sessionId, {
+          maxAge: 1000 * 60 * 30, // 30 min
+          httpOnly: true,
+          secure: true,
+          sameSite: 'strict',
+        });
+      }
 
-    res.cookie('sessionId', sessionId, {
-      maxAge: 1000 * 60 * 30, // 30 min
-      httpOnly: true,
-      secure: true,
-      sameSite: 'strict',
-    });
-    return res.json({
-      success: true,
-      canSubmitFeature: !hasSubmittedFeature,
-      canVote: !hasVoted,
-    });
+      return res.json({
+        success: true,
+        canSubmitFeature: !hasSubmittedFeature,
+        canVote: !hasVoted,
+      });
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        'Internal Server Error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
